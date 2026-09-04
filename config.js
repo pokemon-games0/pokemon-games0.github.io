d61617c3a89a5436672e6585067850b294d490e6
pokemon-games0 <mangalc0032@gmail.com>
2026-09-04 22:21:48 +0500

    Deploy MangaReader site configuration

---
+++ config.js
@@ -0,0 +1,5 @@
+window.MANGAREADER_CONFIG = {
+  SUPABASE_URL: 'https://vcrrkyqiunczhpxdcoil.supabase.co',
+  SUPABASE_ANON_KEY: 'sb_publishable_IWiCOqUZ7PtPaISAo_5RDw_9uTvyD4z',
+  SITE_NAME: 'MangaReader'
+};
