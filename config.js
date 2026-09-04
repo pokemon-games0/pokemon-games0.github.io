b77708a6123840386e56d87aff0967248af50510
pokemon-games0 <mangalc0032@gmail.com>
2026-09-04 22:26:39 +0500

    Remove Supabase upload backend configuration

--- config.js
+++
@@ -1,5 +0,0 @@
-window.MANGAREADER_CONFIG = {
-  SUPABASE_URL: 'https://vcrrkyqiunczhpxdcoil.supabase.co',
-  SUPABASE_ANON_KEY: 'sb_publishable_IWiCOqUZ7PtPaISAo_5RDw_9uTvyD4z',
-  SITE_NAME: 'MangaReader'
-};
